854263eca8ba785c1a9f0644287273cb3c68427a
VitorLima <vitorliima18@gmail.com>
2025-11-20 01:40:38 -0300

    feature: Criando primeiro endpoint no micronaut. HelloWorld controller

---
+++ src/main/java/com/vitorlima/udemy/hello/HelloWorldController.java
@@ -0,0 +1,16 @@
+package com.vitorlima.udemy.hello;
+
+import io.micronaut.http.MediaType;
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Get;
+
+@Controller("/hello")
+public class HelloWorldController {
+
+    @Get(processes = MediaType.TEXT_PLAIN)
+    public String helloWorld(){
+
+        return "Hello World";
+    }
+
+}
