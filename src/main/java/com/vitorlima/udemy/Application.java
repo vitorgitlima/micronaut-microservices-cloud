bf8f7652af5e616515ee33f0429ce48a831ee0a9
VitorLima <vitorliima18@gmail.com>
2025-11-22 16:52:01 -0300

    feature: Adicionando log e modificando contexto no lohback.xml

--- src/main/java/com/vitorlima/udemy/Application.java
+++ src/main/java/com/vitorlima/udemy/Application.java
@@ -1,10 +1,22 @@
 package com.vitorlima.udemy;
 
+import com.vitorlima.udemy.hello.service.HelloMicronautService;
+import com.vitorlima.udemy.hello.service.HelloWorldService;
 import io.micronaut.runtime.Micronaut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Application {
 
+    private static final Logger LOG = LoggerFactory.getLogger(Application.class);
+
     public static void main(String[] args) {
-        Micronaut.run(Application.class, args);
+        var context = Micronaut.run(Application.class, args);
+
+        LOG.debug("Message from service: {}",
+        context.getBean(HelloWorldService.class).helloFromService());
+
+        LOG.debug("Message from service: {}",
+                context.getBean(HelloMicronautService.class).helloFromService());
     }
 }
